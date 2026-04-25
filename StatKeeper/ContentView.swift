4e58e12380622251ff584acf50b96aebd77e9d8f
Kenny Gruchalla <gruchalla@gmail.com>
2026-04-25 16:56:15 -0600

    Initial Commit

---
+++ StatKeeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StatKeeper
+//
+//  Created by Kenny Gruchalla on 4/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StatKeeper/StatKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatKeeperApp.swift
+//  StatKeeper
+//
+//  Created by Kenny Gruchalla on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct StatKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
